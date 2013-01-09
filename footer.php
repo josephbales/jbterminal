2455398b9f4550ed2e7087bb6cc1c9968ecc263c
Joseph Bales <github@josephbales.com>
2013-01-08 21:27:54 -0600

    Original upload.

---
+++ footer.php
@@ -0,0 +1,7 @@
+		<div id="footer">
+			<hr>
+			<p>Some footer stuff</p>
+		</div> <!-- end footer -->
+		<?php wp_footer(); ?>
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,15 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<title><?php if ( is_single() ) { ?><?php wp_title(""); ?> :: <?php } ?><?php bloginfo('name'); ?></title>
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>">
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+		<?php wp_head(); ?>
+	</head>
+	<body>
+		<div id="header">
+			<h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
+		</div> <!-- end header -->
